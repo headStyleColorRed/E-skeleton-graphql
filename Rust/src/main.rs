01edcbfbdaf1b63953686230276b753e21896219
headStyleColorRed <headStyleColorRed@gmail.com>
2021-12-21 22:09:29 +0100

    added actix server

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,13 +1,18 @@
-mod server;
-mod schema;
+use std::io;
+use actix_web:: { web, App, HttpResponse, HttpServer, Responder };
 
-extern crate log;
 
-#[tokio::main]
-async fn main() {
-    let address: [u8; 4] = [127, 0, 0, 1];
-    let port: u16 = 8080;
+fn main()-> io::Result<()> {
+    println!("Running server on port 8080");
 
-    log::info!("Server running on port {}", port);
-    server::start((address, port)).await;
+    HttpServer::new(|| {
+        App::new()
+        .route("/", web::get().to(index))
+    })
+    .bind("localhost:8080")?
+    .run()
+}
+
+fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
 }
