66c179000687aaa33fb01650dc1e41bf46a95aca
headStyleColorRed <headStyleColorRed@gmail.com>
2021-12-21 21:52:47 +0100

    working with single point using warp

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,5 +1,13 @@
 mod server;
+mod schema;
 
-fn main() {
-    health()
+extern crate log;
+
+#[tokio::main]
+async fn main() {
+    let address: [u8; 4] = [127, 0, 0, 1];
+    let port: u16 = 8080;
+
+    log::info!("Server running on port {}", port);
+    server::start((address, port)).await;
 }
--- Rust/src/server/mod.rs
+++ Rust/src/server/mod.rs
@@ -1,1 +1,8 @@
+use std::net::SocketAddr;
+
 mod routes;
+
+// Starts the API server
+pub async fn start(addr: impl Into<SocketAddr>) {
+    warp::serve(routes::make_routes()).run(addr).await;
+}
